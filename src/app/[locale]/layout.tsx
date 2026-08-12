bc92e6902aad6a2857602a75259571034776c41b
aimelebon-98 <306736131+aimelebon-98@users.noreply.github.com>
2026-08-12 22:10:45 +0000

    perf: remove render-blocking Playfair stylesheet, use async @font-face with swap

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -164,9 +164,7 @@ export default async function LocaleLayout({
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
-        />        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700;900&display=swap" rel="stylesheet" />
+        />
       </head>
       <body className="bg-white text-gray-900 antialiased font-sans">
         <TopLoader />
